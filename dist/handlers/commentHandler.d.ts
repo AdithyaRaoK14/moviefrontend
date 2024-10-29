85a0b6c49a6b25b3a2d33d1bad889b002f0de9d8
Adithya Rao Kalathur <117155847+AdithyaRaoK14@users.noreply.github.com>
2024-10-29 11:53:43 +0530

    Add files via upload

---
+++ dist/handlers/commentHandler.d.ts
@@ -0,0 +1,4 @@
+export declare const getComments: (req: any, res: any, next: any) => Promise<void>;
+export declare const createComment: (req: any, res: any, next: any) => Promise<void>;
+export declare const updateComment: (req: any, res: any, next: any) => Promise<void>;
+export declare const deleteComment: (req: any, res: any, next: any) => Promise<void>;
---
+++ dist/handlers/movieHandler.d.ts
@@ -0,0 +1,3 @@
+export declare const getMovie: (req: any, res: any, next: any) => Promise<void>;
+export declare const createMovie: (req: any, res: any, next: any) => Promise<void>;
+export declare const getAllUsers: (req: any, res: any, next: any) => Promise<void>;
